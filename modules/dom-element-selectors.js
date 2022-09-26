1bb4a6253cf0f1472f6e420676fbc0907fc8691f
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2022-09-26 20:31:46 +0200

    include luxon library and modules

---
+++ modules/dom-element-selectors.js
@@ -0,0 +1,7 @@
+export const listSection = document.querySelector('.display-book-section');
+export const formSection = document.querySelector('.form-section');
+export const contactSection = document.querySelector('.contact-section');
+export const navList = document.querySelector('.list');
+export const navForm = document.querySelector('.form');
+export const navContact = document.querySelector('.contact');
+export const collectionSection = document.querySelector('#book-collection');
